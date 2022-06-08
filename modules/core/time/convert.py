f8eca46d8517e9c96aac30834673b94fd73d8268
Elliot <ejohn@udel.edu>
2022-06-08 16:21:19 -0400

    updates

--- modules/core/time/convert.py
+++ modules/core/time/convert.py
@@ -4,8 +4,13 @@ import datetime
 def epoch_time(
         epoch_time,
 ):
+    if not isinstance(epoch_time, (int, float)):
+        raise TypeError(
+            "Error: Argument must be either a float or an"
+            f" integer, instead got type {type(epoch_time)}."
+        )
     res = datetime.datetime.fromtimestamp(epoch_time)
 
     return res
 
-print('testst')
+
