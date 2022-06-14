bf74f600ce92d7598927ddec5650254828777ba5
Elliot <ejohn@udel.edu>
2022-06-14 17:51:20 -0400

    update

--- modules/core/validate/input.py
+++ modules/core/validate/input.py
@@ -19,3 +19,23 @@ def validate_input_number(
             return True
         except ValueError:
             return False
+
+
+def return_all_keys(
+        dictionary,
+):
+    if not isinstance(dictionary, dict):
+        raise TypeError(
+            'Error: Argument must be a dictionary, '
+            f'instead got type {type(dictionary)}'
+        )
+    result = []
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            new_keys = return_all_keys(value)
+            result.append(key)
+            for innerkey in new_keys:
+                result.append(f'{key}/{innerkey}')
+        else:
+            result.append(key)
+    return result
