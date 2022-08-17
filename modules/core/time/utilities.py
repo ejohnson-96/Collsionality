7f439b51ed8d0e536ed34127de33c95378e97f0e
ejohnson-96 <ejohn@udel.edu>
2022-08-17 13:24:34 -0400

    update

--- modules/core/time/utilities.py
+++ modules/core/time/utilities.py
@@ -1,25 +1,29 @@
 import time
 import datetime
-from modules.core.variables.num_man import valid_int
+from modules.core.variables import char_man as cm
+from modules.core.variables.num_man import valid_int, valid_num
+from modules.core.variables import string_man as sm
 
-def validate_time(
-        alarm_time,
+
+def validiate_time(
+        time,
 ):
-    if len(alarm_time) != 11:
+    if len(time) != 11:
         return "Invalid time format! Format HH:MM:SS is required," \
-               f" instead got {alarm_time}. Please try again..."
+               f" instead got {time}. Please try again..."
     else:
-        if int(alarm_time[0:2]) > 24:
+        if int(time[0:2]) > 24:
             return "Invalid HOUR format! Hour argument provided " \
-                   f"{alarm_time[0:2]}. Please try again..."
-        elif int(alarm_time[3:5]) > 59:
+                   f"{time[0:2]}. Please try again..."
+        elif int(time[3:5]) > 59:
             return "Invalid MINUTE format! Minute argument provided " \
-                   f"{alarm_time[3:5]}. Please try again..."
-        elif int(alarm_time[6:8]) > 59:
+                   f"{time[3:5]}. Please try again..."
+        elif int(time[6:8]) > 59:
             return "Invalid SECOND format! Second argument provided " \
-                   f"{alarm_time[6:8]}. Please try again..."
+                   f"{time[6:8]}. Please try again..."
         else:
-            return alarm_time
+            return time
+
 
 def countdown(
         t_,
@@ -46,7 +50,7 @@ def countdown(
 def alarm_clock(
         alarm_time,
 ):
-    validate_time(alarm_time.lower())
+    validiate_time(alarm_time.lower())
     print(f"Setting alarm for {alarm_time}...")
 
     alarm_hour = alarm_time[0:2]
@@ -65,3 +69,200 @@ def alarm_clock(
                         print("Wake Up!")
                         h = 1
     return
+
+
+def datetime_format(
+        entry,
+):
+    entry = sm.valid_string(entry)
+    entry = cm.remove_end(entry)
+    entry = sm.split(entry, 'T')
+
+    date = sm.split(entry[0], '-')
+    time = sm.split(entry[1], ':')
+
+    return datetime.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(float(time[2])))
+
+
+def valid_datetime(
+        entry,
+):
+    if not isinstance(entry, datetime.datetime):
+        raise TypeError(
+            f"Error: Argument {entry}, is not a datetime type, "
+            f"instead got type of {type(entry)}. "
+        )
+    else:
+        return entry
+
+
+def valid_date(
+        entry,
+):
+    if not isinstance(entry, datetime.date):
+        raise TypeError(
+            f"Error: Argument {entry}, is not a datetime.date type, "
+            f"instead got type of {type(entry)}. "
+        )
+    else:
+        return entry
+
+
+def valid_time(
+        entry,
+):
+    if not isinstance(entry, datetime.time):
+        raise TypeError(
+            f"Error: Argument {entry}, is not a datetime.time type, "
+            f"instead got type of {type(entry)}. "
+        )
+    else:
+        return entry
+
+
+def valid_all(
+        entry,
+):
+    if not isinstance(entry, (datetime.time, datetime.datetime, datetime.date)):
+        raise TypeError(
+            f"Error: Argument {entry}, is not a datetime, datetime.time,"
+            f" or datetime.date type. Instead got type of {type(entry)}. "
+        )
+    else:
+        return entry
+
+    return
+
+
+def time_format(
+        entry,
+):
+    entry = valid_datetime(entry)
+    return entry.time()
+
+
+def date_format(
+        entry,
+):
+    entry = valid_datetime(entry)
+    return entry.date()
+
+
+def datetime_epoch(
+        entry,
+):
+    entry = valid_datetime(entry)
+    return (entry - datetime.datetime(1970, 1, 1)).total_seconds()
+
+
+def epoch_datetime(
+        entry,
+):
+    entry = valid_num(entry)
+    return datetime.datetime.fromtimestamp(entry)
+
+
+def time_dif(
+        a,
+        b,
+):
+    a = valid_all(a)
+    b = valid_all(b)
+
+    if type(a) != type(b):
+        raise TypeError(
+            f"Error: Arguments provided, {a} and {b}, must be of the"
+            f" same datetime type. Instead got types of {type(a)} and"
+            f" {type(b)}, respectively."
+        )
+    elif type(a) == datetime.time:
+        date = gen_date(1,1,1971)
+        a = datetime.datetime.combine(date, a)
+        b = datetime.datetime.combine(date, b)
+
+    return b - a
+
+
+def gen_date(
+        day,
+        month,
+        year,
+):
+    for arg_name in (day, month, year):
+        if not isinstance(arg_name, (int, float)):
+            raise TypeError(
+                f"Error: Argument {arg_name}, is not of type int or "
+                f"float. Instead got type of {type(arg_name)}."
+            )
+
+    if len(str(year)) != 4:
+        raise ValueError(
+            f"Error: Argument {year}, is not a valid year. Use "
+            "a 4 digit format and please try again. "
+        )
+    elif month > 12:
+        raise ValueError(
+            f"Error: Argument {month}, cannot be greater than 12."
+        )
+    elif day > 31:
+        raise ValueError(
+            f"Error: Argument {day}, cannot be greater than 31."
+        )
+    else:
+        return datetime.date(year, month, day)
+
+
+def gen_time(
+        hour,
+        min,
+        sec=0,
+        microsec=0,
+):
+    for arg_name in (hour, min, sec, microsec):
+        if not isinstance(arg_name, (int, float, str)):
+            raise TypeError(
+                f"Error: Argument {arg_name}, is not of type int or "
+                f"float. Instead got type of {type(arg_name)}."
+            )
+        if isinstance(arg_name, str):
+            if arg_name.isdigit():
+                continue
+            else:
+                raise ValueError(
+                    f"Error: Argument {arg_name}, is a str type, "
+                    "however, entry is not a valid number. "
+                )
+    if hour > 24:
+        raise ValueError(
+            f"Error: Argument {hour}, cannot be greater than 24."
+        )
+    elif min > 59:
+        raise ValueError(
+            f"Error: Argument {min}, cannot be greater than 59"
+        )
+    elif sec > 59:
+        raise ValueError(
+            f"Error: Argument {sec}, cannot be greater than 59"
+        )
+    elif microsec > 59:
+        raise ValueError(
+            f"Error: Argument {microsec}, cannot be greater than 59"
+        )
+    else:
+        return datetime.time(hour, min, sec, microsec)
+
+
+def gen_datetime(
+        day,
+        month,
+        year,
+        hour,
+        min,
+        sec=0,
+        microsec=0,
+):
+    date = gen_date(day, month, year)
+    time = gen_time(hour, min, sec, microsec)
+
+    return datetime.datetime.combine(date, time)
+
---
+++ modules/core/variables/arr_man.py
@@ -0,0 +1,168 @@
+import numpy as np
+
+
+def valid_arr(
+        inpt,
+):
+    if not isinstance(inpt, np.ndarray):
+        raise TypeError(
+            f"Error: Argument {inpt}, is not an array, "
+            f"instead got type of {type(inpt)}"
+        )
+    else:
+        return inpt
+
+
+def single_arr(
+        arr,
+):
+    arr = valid_arr(arr)
+    if not arr.ndim == 1:
+        raise ValueError(
+            "Error: Argument is not a single length, "
+            f" instead got dimension {arr.ndim}."
+        )
+    else:
+        return arr
+
+
+def arr_dim(
+        arr,
+):
+    arr = valid_arr(arr)
+    return arr.ndim
+
+
+def valid_ent_num(
+        arr,
+):
+    arr = single_arr(arr)
+
+    for i in range(len(arr)):
+        if not isinstance(arr[i], (float, int, np.integer, np.float_)):
+            raise TypeError(
+                f"Error: Entry index {i}; {arr[i]}, is not of type "
+                f"int or float, instead got type of {type(arr[i])}."
+            )
+    else:
+        return arr
+
+
+def valid_ent_str(
+        arr,
+):
+    arr = single_arr(arr)
+
+    for i in range(len(arr)):
+        if not isinstance(arr[i], str):
+            raise TypeError(
+                f"Error: Entry index {i}; {arr[i]}, is not of type "
+                f"string, instead got type of {type(arr[i])}."
+            )
+        else:
+            return arr
+
+
+def valid_ent_bool(
+        arr,
+):
+    arr = single_arr(arr)
+
+    for i in range(len(arr)):
+        if not isinstance(arr[i], (bool, np.bool_)):
+            raise TypeError(
+                f"Error: Entry index {i}; {arr[i]}, is not a boolean "
+                f"type, instead got type of {type(arr[i])}."
+            )
+        else:
+            return arr
+
+
+def equal_len(
+        arr_a,
+        arr_b,
+        show_lengths=False,
+):
+    arr_a = single_arr(arr_a)
+    arr_b = single_arr(arr_b)
+
+    if len(arr_a) != len(arr_b):
+        if show_lengths:
+            print(f'Length - array A: {len(arr_a)}, '
+                  f'array B: {len(arr_b)}.')
+        return False
+    else:
+        if show_lengths:
+            print(f'Length - {len(arr_a)}')
+        return True
+
+
+def print_out(
+        arr,
+        index=False,
+):
+    arr = single_arr(arr)
+
+    for i in range(len(arr)):
+        if index:
+            arg_ = ', ' + str(i) + '.'
+        else:
+            arg_ = ''
+        print(arr[i], arg_)
+    return
+
+
+def sort_asc(
+        arr,
+):
+    arr = single_arr(arr)
+    return np.sort(arr)
+
+
+def sort_dsc(
+        arr,
+):
+    arr = single_arr(arr)
+    return np.sort(arr)[::-1]
+
+
+def first_entry(
+        arr,
+):
+    arr = single_arr(arr)
+    return arr[0]
+
+
+def last_entry(
+        arr,
+):
+    arr = single_arr(arr)
+    return arr[-1]
+
+
+def arr_len(
+        arr,
+):
+    arr = single_arr(arr)
+    return len(arr)
+
+
+def remove_index(
+        arr,
+        index=0,
+):
+    arr = single_arr(arr)
+    return np.delete(arr, index)
+
+
+def remove_last_entry(
+        arr,
+):
+    return remove_index(arr, -1)
+
+
+def remove_first_entry(
+        arr,
+):
+    return remove_index(arr, 0)
+
