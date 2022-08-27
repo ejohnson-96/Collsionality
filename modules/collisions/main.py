4bdf79289624d65658099ed7223ad24c57f05613
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2022-08-27 18:55:04 -0400

    update

---
+++ modules/collisions/main.py
@@ -0,0 +1,7 @@
+from modules.collisions.model.file_gen import load_generate
+
+enc = 6
+radius = 4
+
+x = load_generate(enc, radius)
+print(x)
--- modules/collisions/model/file_gen.py
+++ modules/collisions/model/file_gen.py
@@ -10,24 +10,17 @@ def load_generate(
 
 ):
 
-    encounter = nm.valid_int(encounter)
+    encounter = nm.valid_float(encounter)
+    radius = nm.valid_float(radius)
 
     parent_path = sm.slash_check(fd.dir_parent())
-    parent_path = cm.remove_end(parent_path)
-    name = fd.dir_name(parent_path)
-
-    l = len(name) + 1
-
-    for i in range(l):
-        parent_path = cm.remove_end(parent_path)
-
-    path = sm.jwos(parent_path, slash, 'data', slash, 'save', slash)
+    path = sm.jwos(parent_path, 'data', slash, 'save', slash)
 
     if encounter == 0:
         enc = 'EA'
     else:
         enc = sm.jwos('E', str(encounter))
-
+    print(enc, path)
     fd.dir_make(enc, path)
 
     path = sm.jwos(path, enc, slash)
@@ -37,8 +30,4 @@ def load_generate(
 
 
 
-enc = 6
-radius = 4
 
-x = load_generate(enc, radius)
-print(x)
