f006ab941aa3903c20c3a155726fd70382d6c69c
ejohnson-96 <77989826+ejohnson-96@users.noreply.github.com>
2022-08-29 10:04:14 -0400

    update

--- modules/core/loadsave/file_dir.py
+++ modules/core/loadsave/file_dir.py
@@ -1,8 +1,22 @@
 import os
 import pathlib
 import warnings
+from modules.core.variables.bool_man import valid_bool
+from modules.core.variables.char_man import remove_end
 from modules.core.variables import string_man as sm
 from modules.core.system import config as sys_con
+
+
+def slash(
+
+):
+    if sys_con.windows_os():
+        return '\\'
+    else:
+        return '/'
+
+
+slash = slash()
 
 
 def dir_parent(
@@ -23,109 +37,127 @@ path_dir = dir_path()
 def dir_make(
         name,
         loc=path_dir,
+        warning=True,
 ):
     name = sm.valid_string(name)
-    return sm.slash_check(loc) + name
+    loc = sm.valid_string(loc)
+    warning = valid_bool(warning)
+
+    dir_exists(loc)
+    path = sm.slash_check(loc) + name
+
+    if dir_exists(path, warning):
+        return False
+    else:
+        os.mkdir(path)
+        return True
 
 
 def dir_exists(
-        path_dir,
+        loc,
         warning=False,
 ):
-    path_dir = sm.valid_string(path_dir)
+    loc = sm.valid_string(loc)
+    warning = valid_bool(warning)
 
-    if os.path.exists(path_dir):
-        if warning:
-            warnings.warn("Warning: Directory already exits.")
-        return False
-    else:
+    if os.path.exists(loc):
         return True
+    else:
+        if warning:
+            warnings.warn(
+                f"Warning: Directory {loc}, already exists."
+            )
+        return False
 
 
 def dir_name(
         loc=None,
+        warning=True,
 ):
+    warning = valid_bool(warning)
+
     if isinstance(loc, type(None)):
         return os.path.basename(os.getcwd())
     else:
-        if not isinstance(loc, str):
-            raise TypeError(
-                "Error: Directory path passed must be a string,"
-                f" instead got type {type(loc)}."
-            )
-        isExist = os.path.exists(loc)
-        if isExist:
+        loc = sm.valid_string(loc)
+        if dir_exists(loc, warning):
             return os.path.basename(loc)
-        else:
-            warnings.warn(
-                "Error: Directory location provided does not exist,"
-                f" location provided is: {loc}"
-            )
-            return False
 
 
-def file_list(
-        loc=path_dir,
+def dir_drop(
+        loc,
 ):
-    return next(os.walk(sm.slash_check(loc)))[2]
-
-
-def file_num(
-        loc=path_dir,
-):
-    return len(file_list(loc))
-
-
-def folder_list(
-        loc=path_dir,
-):
-    return next(os.walk(sm.slash_check(loc)))[1]
-
-
-def folder_num(
-        loc=path_dir,
-):
-    return len(folder_list(loc))
+    loc = sm.valid_string(loc)
+    for i in range(len(loc.split(slash)[-1])):
+        loc = remove_end(loc)
+    return loc
 
 
 def dir_list(
         loc=path_dir,
 ):
-    return os.listdir(sm.slash_check(loc))
+    return os.listdir(sm.slash_check(sm.valid_string(loc)))
 
 
 def dir_num(
         loc=path_dir,
 ):
-    return len(dir_list(loc))
+    return len(dir_list(sm.valid_string(loc)))
 
 
-def slash(
-
+def file_exists(
+        loc,
+        warning=False,
 ):
-    if sys_con.windows_os():
-        return '\\'
+    loc = sm.valid_string(loc)
+    warning = valid_bool(warning)
+
+    if os.path.isfile(loc):
+        return True
     else:
-        return '/'
+        if warning:
+            warnings.warn(
+                f"Warning: The file {loc}, does not exits."
+            )
+        return False
+
+
+def file_list(
+        loc=path_dir,
+):
+    return next(os.walk(sm.slash_check(sm.valid_string(loc))))[2]
+
+
+def file_num(
+        loc=path_dir,
+):
+    return len(file_list(sm.valid_string(loc)))
+
+
+def folder_list(
+        loc=path_dir,
+):
+    return next(os.walk(sm.slash_check(m.valid_string(loc))))[1]
+
+
+def folder_num(
+        loc=path_dir,
+):
+    return len(folder_list(sm.valid_string(loc)))
 
 
 def file_ext(
         loc=path_dir,
 ):
-    if not isinstance(loc, str):
-        raise TypeError(
-            'Error: File location must be of type string,'
-            f' instead got type of {type(loc)}.'
-        )
-
-    return os.path.splitext(loc)[-1].lower()
+    return os.path.splitext(sm.valid_string(loc))[-1].lower()
 
 
 def all_file_type(
         file_type,
         loc=path_dir,
 ):
-    loc = sm.slash_check(loc)
+    file_type = sm.valid_string(file_type)
+    loc = sm.valid_string(sm.slash_check(loc))
     res = []
     for root, dirs, files in os.walk(loc):
         for file in files:
@@ -137,5 +169,4 @@ def all_file_type(
 def load_bar(
 
 ):
-
     return
